33e3ec6ccdf5706d92c09c18b0907d4e98762554
Brien Thomas <thomasbrien94@gmail.com>
2022-06-21 12:51:57 -0700

    shop list and taco shop components correctly render out an item for each taco shop in the table

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { getMovies, getPosts, getTacos } from './services/fetch-utils';
 import MoviesList from './MoviesList';
 import PostsList from './PostsList';
-
+import ShopList from './ShopList';
 
 import './App.css';
 // import your arrays here
@@ -11,6 +11,7 @@ function App() {
 
   const [movies, setMovies] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [tacoShops, setTacoShops] = useState([]);
 
   async function fetchMoviesData() {
     const movies = await getMovies();
@@ -23,15 +24,25 @@ function App() {
     setPosts(posts);
   }
 
+  async function fetchTacos() {
+    const shops = await getTacos();
+    setTacoShops(shops);
+  }
+
   useEffect(() => {
     fetchMoviesData();
     fetchPosts();
+    fetchTacos();
   }, []);
 
   return (
     <div className="App">
       <h2>Posts List</h2>
       <PostsList posts={posts} />
+      <hr />
+
+      <h2>Tacos List</h2>
+      <ShopList tacoShops={tacoShops} />
       <hr />
 
       <h2>Movies List</h2>
